8bac5cca2d49ba0e9f9d7cfb75ee7dae98586f67
Harry Ng <harryworld@gmail.com>
2016-01-16 12:24:44 +0800

    Add NotDoingItem

---
+++ NotDoingList/CollectionView/NotDoingItem.swift
@@ -0,0 +1,18 @@
+//
+//  NotDoingItem.swift
+//  NotDoingList
+//
+//  Created by Harry Ng on 16/1/2016.
+//  Copyright © 2016 STAY REAL. All rights reserved.
+//
+
+import Cocoa
+
+class NotDoingItem: NSCollectionViewItem {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
--- NotDoingList/ViewController.swift
+++ NotDoingList/ViewController.swift
@@ -10,18 +10,42 @@ import Cocoa
 
 class ViewController: NSViewController {
 
+    @IBOutlet weak var collectionView: NSCollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
 
-    override var representedObject: AnyObject? {
-        didSet {
-        // Update the view, if already loaded.
-        }
-    }
-
-
 }
 
+extension ViewController: NSCollectionViewDataSource {
+    func numberOfSectionsInCollectionView(collectionView: NSCollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(collectionView: NSCollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(collectionView: NSCollectionView, itemForRepresentedObjectAtIndexPath indexPath: NSIndexPath) -> NSCollectionViewItem {
+        let item = collectionView.makeItemWithIdentifier("NotDoingItem", forIndexPath: indexPath)
+        
+        item.textField?.stringValue = "Not Doing List \(indexPath.item)"
+        
+        return item
+    }
+}
+
+extension ViewController: NSCollectionViewDelegate {
+    
+}
+
+extension ViewController: NSCollectionViewDelegateFlowLayout {
+    
+    func collectionView(collectionView: NSCollectionView, layout collectionViewLayout: NSCollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> NSSize {
+        return NSSize(width: collectionView.bounds.width, height: 50)
+    }
+    
+}
